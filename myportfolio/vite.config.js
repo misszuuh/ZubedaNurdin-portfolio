45b0a44f9bb0ba06b4ef371f936cfadc5b0c6e4a
DeveloperBhin <norbeth559@gmail.com>
2025-12-09 08:40:01 +0300

    modify

--- myportfolio/vite.config.js
+++ myportfolio/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/', // very important for correct routing
   server: {
     watch: {
       usePolling: true,
